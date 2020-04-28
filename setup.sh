b24d910bd780e1b970c6893cc930e45d0aaff31b
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2020-04-27 21:55:42 -0300

    changes

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"paulo_calado@live.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
